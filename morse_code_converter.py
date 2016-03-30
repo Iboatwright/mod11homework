35bcbd566fbfe127339d9a5baaf09cc17e2d808a
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-03-30 18:43:59 -0400

    Pseudocode is complete.  mcc.txt file still needs some garbage notes removed.

--- morse_code_converter.py
+++ morse_code_converter.py
@@ -2,13 +2,23 @@
 # Exercise Selected: Chapter 11 program 9
 # Name of program: Morse Code Converter
 # Description of program:  This program takes a string of characters
-#   input by the user and converts the string to morse code.  It then
+#   input by the user and converts the string to Morse Code.  It then
 #   displays the converted string to the user.
 
 
 def main():
     # Local variables
     results = ''
+    
+    # Morse Code dictionary
+    morse = {'0':'-----','1':'.----','2':'..---','3':'...--','4':'....-',
+             '5':'.....','6':'-....','7':'--...','8':'---..','9':'----.',
+             'a':'.-','b':'-...','c':'-.-.','d':'-..','e':'.','f':'..-.',
+             'g':'--.','h':'....','i':'..','j':'.---','k':'-.-','l':'.-..',
+             'm':'--','n':'-.','o':'---','p':'.---.','q':'--.-','r':'.-.',
+             's':'...','t':'-','u':'..-','v':'...-','w':'.--','x':'-..-',
+             'y':'-.--','z':'--..',' ':' ',',':'--..--','.':'.-.-.-',
+             '?':'..--..'}
 
     # Display the intro to the user.
     fluffy_intro()
@@ -26,6 +36,11 @@ def fluffy_intro():
     print('This program converts a string of characters into morse code'
           ' and displays the results.')
     return None
+
+
+# Returns a string used to identify a new part(i.e. page) of the program.
+def page_header(title):
+    return '{0:-<62}\n\n{1:^67}\n{0:_<62}\n'.format('    ', title)
 
 
 # Displays the summation results to the user.
